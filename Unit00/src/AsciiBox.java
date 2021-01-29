60c899d350d7eacb8facc2fc71030a8617d8b46d
mayarosenbaum <mayakrosenbaum@gmail.com>
2021-01-28 20:32:32 -0800

    completed 001 projects

---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name: Maya Rosenbaum
+//Date: 1/28/21
+//Class: APCSA Period 3
+//Lab: AsciiBox.java
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name \t  date \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+}
---
+++ Unit00/src/Variables.java
@@ -0,0 +1,70 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name: Maya Rosenbaum
+//Date: 1/28/21
+//Class: APCSA Period 3
+//Lab: Variables.java
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 26;
+		int intOne = 42;
+		long longOne = 4666;
+
+		//decimal variables
+		float floatOne = 46;
+		double doubleOne = 22.8;
+
+		//other integer types
+
+		//other types
+		char charOne = 'm';
+		String stringOne = new String("Maya");
+		boolean booleanOne = true;
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*2 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*4 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*8 bit - longOne = "+longOne+"\t\t*");
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*      decimal variables        *");
+		System.out.println("*                               *");
+		System.out.println("*4 bit - floatOne = "+floatOne+"\t\t*");
+		System.out.println("*8 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*      other types        *");
+		System.out.println("*                               *");
+		System.out.println("*2 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*----- - stringOne = "+stringOne+"\t\t*");
+		System.out.println("*1 bit - booleanOne = "+booleanOne+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+	}
+}
