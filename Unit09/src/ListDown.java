7b05a3031e568793a2c8db1fe71a3885d1a3ee6c
mayarosenbaum <mayakrosenbaum@gmail.com>
2021-03-10 16:58:02 -0800

    finished Unit 9

---
+++ Unit09/src/ListDown.java
@@ -0,0 +1,26 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListDown
+{
+	//go() will return true if all numbers in numArray
+	//are in decreasing order [31,12,6,2,1]
+	public static boolean go(List<Integer> numArray)
+	{
+		for (int i = 0; (i < numArray.size() && i+1< numArray.size()); i++) {
+			if (numArray.get(i) > numArray.get(i+1)) {
+				return true;
+			}
+			else {
+				return false;
+		}
+			
+	}
+		return false;
+}
+}
