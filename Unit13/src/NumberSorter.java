60d2c0bcf41c7d7192cb0a5813290bd30b4eda04
mayarosenbaum <mayakrosenbaum@gmail.com>
2021-04-21 13:13:13 -0700

    unit13 (finish wordsearch)

--- Unit13/src/NumberSorter.java
+++ Unit13/src/NumberSorter.java
@@ -2,25 +2,61 @@
 //www.apluscompsci.com
 //Name -
 
-import java.util.Arrays; 
-import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
-import static java.lang.System.*;
+import java.util.Arrays;
 
 public class NumberSorter
 {
-	//instance variables and other methods not shown
-
-	private static int getNumDigits(int number)
-	{
-		int count = 0;
-		return count;
-	}
-
-	public static int[] getSortedDigitArray(int number)
-	{
-		int[] sorted = null;
-		return sorted;
-	}
+    static int count = 0;
+    private static int getNumDigits(int number)
+    {
+        while(number>0){
+ 
+                number=number/10;
+            count++;
+        }
+        return count;
+    }
+ 
+    public static int[] getSortedDigitArray(int number)
+    {
+        int[] sorted = new int[getNumDigits(number)];
+        for(int a = 0; a<sorted.length; a++)
+        {
+            sorted[a] = number%10;
+            number = number/10;
+        }
+        for(int a = 0; a<sorted.length; a++)
+        {
+            int y = a;
+            for(int b = a+1; b<sorted.length; b++)
+            {
+                if(sorted[b]<sorted[y])
+                {
+ 
+                    y = b;
+                }
+            }
+            if(y != a)
+            {
+                int temp = sorted[y];
+                sorted[y]=sorted[a];
+                sorted[a]=temp;
+            }
+        }
+        return sorted;
+    }
+ 
+    public static void main(String args[])
+    {
+        int[] cases = {567891, 901912468, 864213507, 898777, 234422, 29826};
+        for( int test : cases )
+        {
+            int[] one = NumberSorter.getSortedDigitArray( test );
+            for(int item : one)
+            {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- Unit13/src/WordSortTwo.java
+++ Unit13/src/WordSortTwo.java
@@ -8,19 +8,113 @@ import static java.lang.System.*;
 
 public class WordSortTwo
 {
-	private String[] wordRay;
+    // instance variable
+    private String[] wordRay;
 
-	public WordSortTwo(String sentence)
-	{
-	}
+    
+    // init constructor
+    public WordSortTwo(String sentence)
+    {
+        // creates an array of the words in sentence
+        wordRay = sentence.split(" ");
+        
+        // sort the array of words alphabetically
+        bubbleSort();
+        
+    }
 
-	public void sort()
-	{
-	}
+    
+    public void resetWords(String sentence)
+    {
+        // creates an array of the words in sentence
+        wordRay = sentence.split(" ");
+        
+        // sorts the array of words alphabetically
+        selectionSort();
+    }
+    
+    
+    public void bubbleSort()
+    {
+        // use your own sort
+        // Don't use Arrays.sort(wordRay);
+      
+        // you will need an outer loop to make wordRay.length passes
+        for (int p=1; p < wordRay.length; p++)
+        {
+            // you will need an inner loop that will
+            // compare each i and i+1 element to see if they are out of order (OOO)
+            for (int i=0; i < wordRay.length-1; i++)
+            {
+                 // check to see if the i and i+1 elements are out of order (OOO)
+                 // use the compareTo() method to see if
+                 // wordRay[i].compareTo(wordRay[i+1]) is greater than 0
+                 // and if so, swap the i and i+1 elements
+                 if (wordRay[i].compareTo(wordRay[i+1]) > 0) // use greater than  OOO
+                 {
+                      // HERE is some general info
+                      // swap the ith and i+1 elements
+                      // use a temporary variable to help with your swap
+                   
+                      // START HERE with your code
+                      // STEP 1: copy the i element to the temp box
+                      String temp = wordRay[i];
+                      
+                      // copy the i+1 element to the i box
+                      wordRay[i] = wordRay[i+1];
+                      
+                      // copy the temp element to the i+1 box
+                      wordRay[i+1] = temp;
+                   
+                 }  // end of if
+              
+            } // end of for i loop to loop through the words
+        } // end of outer for loop for the number of passes
+    }
 
-	public String toString()
-	{
-		String output="";
-		return output+"\n\n";
-	}
+    public void selectionSort()
+    {
+        // write a selection sort to sort the words
+        // GOOGLE IT!
+        for ( int j=0; j < wordRay.length-1; j++ )
+    {
+      // Find min: the index of the string reference that should go into cell j.
+      // Look through the unsorted strings (those at j or higher) for the one that is first in lexicographic order
+      int min = j;
+      for ( int k=j+1; k < wordRay.length; k++ )
+        if ( wordRay[k].compareTo( wordRay[min] ) < 0 ) min = k;  
+
+      // Swap the reference at j with the reference at min 
+      String templ = wordRay[j];
+      wordRay[j] = wordRay[min];
+      wordRay[min] = templ;
+    }
+      
+    }
+    
+    
+    
+    public String toString()
+    {
+        String output="";
+
+        // join each word in the wordRay list to your output
+        // you will need to loop through the list adding all the way
+        // if it is the last element
+        //     output += the word; // no ", " 
+        // else
+        //     output += the word + ", "; 
+
+        // loop through all of the wordRay objects
+        for (int i = 0; i<wordRay.length; i++)
+        {
+           if (i == wordRay.length-1) // last index
+                output += wordRay[i]; // no ", " 
+            else
+             output += wordRay[i] + ", ";
+        }
+        
+        return output; // fix me!!!! by returning your output
+    }
+    
 }
