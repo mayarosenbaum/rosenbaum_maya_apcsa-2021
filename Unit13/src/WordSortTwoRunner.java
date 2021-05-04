0d04b9524ddaebed6c45ba027bd66c8392cd8639
mayarosenbaum <mayakrosenbaum@gmail.com>
2021-05-04 08:44:38 -0700

    first commit

--- Unit13/src/WordSortTwoRunner.java
+++ Unit13/src/WordSortTwoRunner.java
@@ -13,12 +13,22 @@ public class WordSortTwoRunner
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner(new File("wordsorttwo.dat"));
-		int size = file.nextInt();
-		file.nextLine();
-		for(int i = 0; i<size; i++)
-		{
-			String sentence = file.nextLine();
-			//instantiate a new WordSort
-		}
-	}
+        
+        int size = file.nextInt(); // reads in first line (# of sentences)
+        file.nextLine(); // moves file cursor to the next line
+        
+        for(int i = 0; i < size; i++)
+        {
+            String sentence = file.nextLine(); // reads in the sentence
+            System.out.println(sentence);
+            
+            // instantiate a new WordSortTwo object
+            WordSortTwo words = new WordSortTwo(sentence);
+            System.out.println(words);  // calls toString() method
+            words.resetWords(sentence);
+            System.out.println(words);  // calls toString() method            
+            System.out.println();
+        }
+        
+    } // end of main method
 }
